cb6b4df66a89c4cecbb1664a3461e355795c9b1c
Federico Capoano <nemesis@ninux.org>
2018-09-19 09:55:39 +0200

    [tests] Updated name of test sqlite db

--- tests/settings.py
+++ tests/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'freeradius.db'),
+        'NAME': os.path.join(BASE_DIR, 'openwisp_radius.db'),
     }
 }
 
