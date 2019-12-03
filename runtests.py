10d968652af1677484306700b47786cfa5b110c4
NeoxN3il <57082943+X3NOSIZ@users.noreply.github.com>
2019-12-03 23:21:15 +0530

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -8,7 +8,7 @@ sys.path.insert(0, "tests")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 if __name__ == "__main__":
-    from django.core.management import execute_from_command_line
+    from django3.core.management import execute_from_command_line
     args = sys.argv
     args.insert(1, "test")
     args.insert(2, "openwisp_radius")
