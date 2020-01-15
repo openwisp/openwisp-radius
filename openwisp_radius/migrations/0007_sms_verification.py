d781c87b9bf89a8a6fd24cf9e96618963ffcf1a1
Federico Capoano <federico.capoano@gmail.com>
2020-01-15 14:54:46 +0100

    [sms] Update SMS db migration ancestor

--- openwisp_radius/migrations/0007_sms_verification.py
+++ openwisp_radius/migrations/0007_sms_verification.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('openwisp_radius', '0005_radiustoken'),
+        ('openwisp_radius', '0006_add_radactt_fields'),
         ('openwisp_users', '0005_user_phone_number'),
     ]
 
