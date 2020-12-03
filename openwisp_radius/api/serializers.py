openwisp/openwisp-radius

commit 4cad64492fb7a821156c7c838018b7690ba2b883

[change] Disabled phone token API endpoints if SMS verification not enabled #189
If an organization does not enable SMS verification, the API endpoints
related to the mobile phone number will be turned off.

Closes #189

openwisp_radius/api/serializers.py
@@ -28,7 +28,7 @@ from rest_framework.exceptions import APIException
 from .. import settings as app_settings
 from ..base.forms import PasswordResetForm
 from ..utils import load_model
-from .utils import ErrorDictMixin
+from .utils import ErrorDictMixin, is_sms_verification_enabled
 
 logger = logging.getLogger(__name__)
 
@@ -326,22 +326,9 @@ class RegisterSerializer(
         default='',
     )
 
-    @property
-    def is_sms_verification_enabled(self):
-        org = self.context['view'].organization
-        try:
-            return org.radius_settings.sms_verification
-        except ObjectDoesNotExist:
-            raise APIException(
-                _(
-                    'Could not complete operation '
-                    'because of an internal misconfiguration'
-                )
-            )
-
     def validate_phone_number(self, phone_number):
         org = self.context['view'].organization
-        if self.is_sms_verification_enabled:
+        if is_sms_verification_enabled(org):
             if not phone_number:
                 raise serializers.ValidationError(_('This field is required'))
             mobile_prefixes = org.radius_settings.allowed_mobile_prefixes_list
@@ -349,6 +336,9 @@ class RegisterSerializer(
                 raise serializers.ValidationError(
                     _('This international mobile prefix is not allowed.')
                 )
+        else:
+            # Phone number should not be stored if sms verification is disabled
+            phone_number = None
         return phone_number
 
     def save(self, request):
@@ -367,7 +357,7 @@ class RegisterSerializer(
         if phone_number != self.fields['phone_number'].default:
             user.phone_number = phone_number
         org = self.context['view'].organization
-        if self.is_sms_verification_enabled:
+        if is_sms_verification_enabled(org):
             user.is_active = False
         try:
             user.full_clean()

openwisp_radius/api/permissions.py
@@ -1,0 +1,27 @@
+import logging
+
+from django.utils.translation import gettext_lazy as _
+from ipware import get_client_ip
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import BasePermission
+
+from .utils import is_sms_verification_enabled
+
+logger = logging.getLogger(__name__)
+
+
+class IsSmsVerificationEnabled(BasePermission):
+    def has_permission(self, request, view):
+        organization = getattr(view, 'organization')
+        client_ip = get_client_ip(request)[0]
+        verification = is_sms_verification_enabled(organization)
+        if not verification:
+            logger.warning(
+                f'View {view.__class__.__name__} is being accessed for organization '
+                f'{organization.name} but SMS verification is disabled for '
+                f'this organization. Client IP address: {client_ip}'
+            )
+            raise PermissionDenied(
+                _('SMS verification is not enabled for this organization')
+            )
+        return verification

openwisp_radius/api/utils.py
@@ -1,6 +1,27 @@
+import logging
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import APIException
+
+logger = logging.getLogger(__name__)
+
+
 class ErrorDictMixin(object):
     def _get_error_dict(self, error):
         dict_ = error.message_dict.copy()
         if '__all__' in dict_:
             dict_['non_field_errors'] = dict_.pop('__all__')
         return dict_
+
+
+def is_sms_verification_enabled(org):
+    try:
+        return org.radius_settings.sms_verification
+    except ObjectDoesNotExist:
+        logger.exception(
+            f'Got exception while accessing radius_settings for {org.name}'
+        )
+        raise APIException(
+            _('Could not complete operation ' 'because of an internal misconfiguration')
+        )

openwisp_radius/api/views.py
@@ -58,6 +58,7 @@ from openwisp_users.api.permissions import IsOrganizationManager
 from .. import settings as app_settings
 from ..exceptions import PhoneTokenException
 from ..utils import generate_pdf, load_model
+from .permissions import IsSmsVerificationEnabled
 from .serializers import (
     AuthorizeSerializer,
     AuthTokenSerializer,
@@ -812,7 +813,10 @@ class CreatePhoneTokenView(
     ErrorDictMixin, BaseThrottle, DispatchOrgMixin, CreateAPIView
 ):
     authentication_classes = (InactiveBearerTokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (
+        IsSmsVerificationEnabled,
+        IsAuthenticated,
+    )
 
     def create(self, *args, **kwargs):
         request = self.request
@@ -835,7 +839,10 @@ create_phone_token = CreatePhoneTokenView.as_view()
 
 class ValidatePhoneTokenView(DispatchOrgMixin, GenericAPIView):
     authentication_classes = (InactiveBearerTokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (
+        IsSmsVerificationEnabled,
+        IsAuthenticated,
+    )
     serializer_class = ValidatePhoneTokenSerializer
 
     def _error_response(self, message, key='non_field_errors', status=400):
@@ -888,7 +895,10 @@ validate_phone_token = ValidatePhoneTokenView.as_view()
 )
 class ChangePhoneNumberView(CreatePhoneTokenView):
     authentication_classes = (InactiveBearerTokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (
+        IsSmsVerificationEnabled,
+        IsAuthenticated,
+    )
     serializer_class = ChangePhoneNumberSerializer
 
     def create(self, *args, **kwargs):

openwisp_radius/tests/test_api.py
@@ -2576,3 +2576,63 @@ class TestApiPhoneToken(ApiTokenMixin, BaseTestCase):
         # is_active state of user should not change because an error
         # occurred during phone token creation.
         self.assertTrue(user.is_active)
+
+
+class TestIsSmsVerificationEnabled(ApiTokenMixin, BaseTestCase):
+
+    _test_email = 'test@openwisp.org'
+
+    def setUp(self):
+        super().setUp()
+        radius_settings = self.default_org.radius_settings
+        radius_settings.sms_verification = False
+        radius_settings.full_clean()
+        radius_settings.save()
+
+    def test_register_201_phone_number_empty(self):
+        self._superuser_login()
+        self.assertEqual(User.objects.count(), 1)
+        url = reverse('radius:rest_register', args=[self.default_org.slug])
+        phone_number = '+393664255801'
+        r = self.client.post(
+            url,
+            {
+                'username': self._test_email,
+                'email': self._test_email,
+                'password1': 'password',
+                'password2': 'password',
+                'phone_number': phone_number,
+            },
+        )
+        self.assertEqual(r.status_code, 201)
+        self.assertIn('key', r.data)
+        self.assertEqual(User.objects.count(), 2)
+        user = User.objects.get(email=self._test_email)
+        self.assertTrue(user.is_member(self.default_org))
+        self.assertEqual(user.phone_number, None)
+        self.assertTrue(user.is_active)
+
+    def test_create_phone_token_403(self):
+        url = reverse('radius:phone_token_create', args=[self.default_org.slug])
+        r = self.client.post(url)
+        self.assertEqual(r.status_code, 403)
+        self.assertIn('SMS verification is not enabled', str(r.data))
+
+    def test_validate_phone_token_403(self):
+        url = reverse('radius:phone_token_validate', args=[self.default_org.slug])
+        r = self.client.post(url)
+        self.assertEqual(r.status_code, 403)
+        self.assertIn('SMS verification is not enabled', str(r.data))
+
+    def test_change_phone_number_403(self):
+        url = reverse('radius:phone_number_change', args=[self.default_org.slug])
+        r = self.client.post(url)
+        self.assertEqual(r.status_code, 403)
+        self.assertIn('SMS verification is not enabled', str(r.data))
+
+    def test_missing_radius_settings(self):
+        self.default_org.radius_settings.delete()
+        url = reverse('radius:phone_token_create', args=[self.default_org.slug])
+        r = self.client.post(url)
+        self.assertEqual(r.status_code, 500)
+        self.assertIn('Could not complete operation', str(r.data))

commit 312fdd5a3302f984a7582959c9fcaf73b3b9eb2a

[chores] Fixed minor flake8 issues introduced in #190
Related to #190

openwisp_radius/api/serializers.py
@@ -14,7 +14,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import AllowAllUsersModelBackend
 from django.contrib.auth.models import Group
-from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.core.exceptions import ValidationError
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -23,7 +23,6 @@ from rest_framework import serializers
 from rest_framework.authtoken.serializers import (
     AuthTokenSerializer as BaseAuthTokenSerializer,
 )
-from rest_framework.exceptions import APIException
 
 from .. import settings as app_settings
 from ..base.forms import PasswordResetForm