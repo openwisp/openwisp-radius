5e7d914f0e4ff3e36bf34602dca757f8cd60eea0
Gagan Deep <pandafy.dev@gmail.com>
2021-07-06 16:01:24 +0530

    [fix] Fixed get_url_or_path utility function

--- openwisp_radius/saml/utils.py
+++ openwisp_radius/saml/utils.py
@@ -3,6 +3,6 @@ from urllib.parse import urlparse
 
 def get_url_or_path(url):
     parsed_url = urlparse(url)
-    if parsed_url.hostname:
-        return f'{parsed_url.scheme}://{parsed_url.hostname}'
+    if parsed_url.netloc:
+        return f'{parsed_url.scheme}://{parsed_url.netloc}'
     return parsed_url.path
