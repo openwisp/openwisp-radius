3c0828994f6d77359697a92fe4f91bf5cf543ee1
Vaishnav Nair <nairvaishnav007@icloud.com>
2021-09-30 00:44:03 +0530

    [requested-change] Added tests

--- openwisp_radius/tests/test_api/test_utils.py
+++ openwisp_radius/tests/test_api/test_utils.py
@@ -3,7 +3,7 @@ from rest_framework.exceptions import APIException
 from openwisp_utils.tests import capture_any_output
 
 from ... import settings as app_settings
-from ...api.utils import is_registration_enabled
+from ...api.utils import is_registration_enabled, is_sms_verification_enabled
 from ...utils import load_model
 from ..mixins import BaseTestCase
 
@@ -35,6 +35,33 @@ class TestUtils(BaseTestCase):
             org.radius_settings = None
             with self.assertRaises(APIException) as context_manager:
                 is_registration_enabled(org)
+            self.assertEqual(
+                str(context_manager.exception),
+                'Could not complete operation because of an internal misconfiguration',
+            )
+
+    def test_is_sms_verification_enabled(self):
+        org = self._create_org()
+        OrganizationRadiusSettings.objects.create(organization=org)
+
+        with self.subTest('Test sms verification enabled set to True'):
+            org.radius_settings.sms_verification_enabled = True
+            self.assertEqual(is_sms_verification_enabled(org), True)
+
+        with self.subTest('Test sms verification enabled set to False'):
+            org.radius_settings.sms_verification_enabled = False
+            self.assertEqual(is_sms_verification_enabled(org), False)
+
+        with self.subTest('Test sms verification enabled set to None'):
+            org.radius_settings.sms_verification_enabled = None
+            self.assertEqual(
+                is_sms_verification_enabled(org), app_settings.SMS_DEFAULT_VERIFICATION,
+            )
+
+        with self.subTest('Test related radius setting does not exist'):
+            org.radius_settings = None
+            with self.assertRaises(APIException) as context_manager:
+                is_sms_verification_enabled(org)
             self.assertEqual(
                 str(context_manager.exception),
                 'Could not complete operation because of an internal misconfiguration',
