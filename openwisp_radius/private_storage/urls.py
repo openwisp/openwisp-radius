1cc74d17fa41a0732c409dbef6e25aeb606db416
Aryamanz29 <aryamanz29@gmail.com>
2022-10-17 11:51:53 +0530

    [fix] Use "path" URL kwarg in "serve_private_file" URL pattern #418
    Fixes #418

--- openwisp_radius/private_storage/urls.py
+++ openwisp_radius/private_storage/urls.py
@@ -9,7 +9,11 @@ from . import views
 def get_private_store_urls():
     return [
         path(
-            urljoin(app_settings.CSV_URL_PATH, '<path:csvfile>'),
+            # Use "path" URL kwarg to make it consistent with
+            # django-private-storage. Otherwise, the S3 reverse
+            # proxy feature of django-private-storage does
+            # not work.
+            urljoin(app_settings.CSV_URL_PATH, '<path:path>'),
             views.rad_batch_csv_download_view,
             name='serve_private_file',
         )
