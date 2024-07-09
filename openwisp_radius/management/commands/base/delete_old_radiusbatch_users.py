c7552525e6f193c0ee69c223056aece3d09cb3a2
kaushikaryan04 <kaushik.aryan04@gmail.com>
2024-07-09 19:56:36 +0530

    [change] Changed the delete_old_radiusbatch_users command to use days as well #525
    Added the option to delete users by specifying days or months.If nothing is provided 18 months will be taken as default.
    
    Fixes #525

--- openwisp_radius/management/commands/base/delete_old_radiusbatch_users.py
+++ openwisp_radius/management/commands/base/delete_old_radiusbatch_users.py
@@ -16,15 +16,29 @@ class BaseDeleteOldRadiusBatchUsersCommand(BaseCommand):
         parser.add_argument(
             '--older-than-days',
             action='store',
+            type=int,
+            help='Delete users that are older than days provided',
+        )
+        parser.add_argument(
+            '--older-than-months',
+            action='store',
+            type=int,
             default=BATCH_DELETE_EXPIRED,
-            help='delete users which have expired before this time',
+            help='Delete users that are older than months provided',
         )
 
     def handle(self, *args, **options):
-        days = now() - timedelta(days=int(options['older_than_days']))
-        batches = RadiusBatch.objects.filter(expiration_date__lt=days)
+        days = options.get('older_than_days')
+        months = options.get('older_than_months')
+
+        if days is not None:
+            threshold_date = now() - timedelta(days=days)
+        else:
+            threshold_date = now() - timedelta(days=30 * months)
+
+        batches = RadiusBatch.objects.filter(expiration_date__lt=threshold_date)
+        time_period = threshold_date.strftime('%Y-%m-%d %H:%M:%S')
+
         for b in batches:
             b.delete()
-        self.stdout.write(
-            f'Deleted accounts older than {options["older_than_days"]} days'
-        )
+        self.stdout.write(f'Deleted accounts older than {time_period}')
