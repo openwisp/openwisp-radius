4d91784a6680324682def04560da235d78d7da6b
Federico Capoano <f.capoano@openwisp.io>
2024-07-17 15:23:03 -0400

    [deps] Pin pydyf~=0.10.0 (0.11.0 breaks)
    We can remove this once we can update wasyprint.

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'passlib~=1.7.1',
         'djangorestframework-link-header-pagination~=0.1.1',
         'weasyprint~=59.0',
+        'pydyf~=0.10.0',  # remove this once we upgrade wasyprint
         'dj-rest-auth~=4.0.1',
         'django-sendsms~=0.5.0',
         'jsonfield~=3.1.0',
