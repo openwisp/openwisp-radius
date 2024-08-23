fb8231ddf66e5c15ff63215aff3728e5760694bb
Federico Capoano <f.capoano@openwisp.io>
2024-08-23 11:02:29 -0400

    [tests] Added allauth.account.middleware.AccountMiddleware

--- tests/openwisp2/settings.py
+++ tests/openwisp2/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'sesame.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
