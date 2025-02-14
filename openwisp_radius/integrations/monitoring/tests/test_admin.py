2b32b899954cc64ec346c3dfc2e2e60528ea6fc6
Gagan Deep <pandafy.dev@gmail.com>
2025-02-15 00:48:42 +0530

    [fix] Improved query for RADIUS monitoring dashboard charts
    Previously, the queries performed datetime interpolation at the database level,
    which prevented the database from efficiently using indexes. This change ensures
    that filters use UTC datetime objects directly, allowing the database to leverage
    indexes for improved query performance.
    
    Fixed also the target_url for "Today's RADIUS Session" chart.
    
    (cherry picked from commit 0ddf2a67ab5c698da6e6702b3ca7c1c6987d8191)

--- openwisp_radius/integrations/monitoring/tests/test_admin.py
+++ openwisp_radius/integrations/monitoring/tests/test_admin.py
@@ -1,16 +1,20 @@
-from django.test import TestCase, tag
+from django.test import TestCase, override_settings, tag
 from django.urls import reverse
-from django.utils import timezone
+from freezegun import freeze_time
 
 from openwisp_radius.tests import _RADACCT, CreateRadiusObjectsMixin
 
-from ..utils import get_datetime_filter_start_date, get_datetime_filter_stop_date
+from ..utils import (
+    get_datetime_filter_start_datetime,
+    get_datetime_filter_stop_datetime,
+)
 from .mixins import CreateDeviceMonitoringMixin
 
 
 @tag('radius_monitoring')
 class TestDeviceAdmin(CreateRadiusObjectsMixin, CreateDeviceMonitoringMixin, TestCase):
     app_label = 'config'
+    radius_label = 'openwisp_radius'
 
     def setUp(self):
         admin = self._create_admin()
@@ -31,16 +35,26 @@ class TestDeviceAdmin(CreateRadiusObjectsMixin, CreateDeviceMonitoringMixin, Tes
             '<tbody id="radius-session-tbody"></tbody>',
         )
 
+    @freeze_time('2025-02-13 00:00:00+05:30')
+    @override_settings(TIME_ZONE='Asia/Kolkata')
     def test_radius_dashboard_chart_data(self):
         options = _RADACCT.copy()
+        # Create RADIUS Accounting with UTC time
         options['unique_id'] = '117'
-        options['start_time'] = timezone.now().strftime('%Y-%m-%d 00:00:00')
+        options['start_time'] = '2025-02-12T18:29:00+00:00'
         options['input_octets'] = '1234567890'
+        # Create RADIUS Accounting with IST time
+        self._create_radius_accounting(**options)
+        options['unique_id'] = '118'
+        options['start_time'] = '2025-02-13 00:00:00+05:30'
+        # The dashboard queries RadiusAccounting with localdate,
+        # therefore, it should not return the RADIUS Accounting created
+        #  with UTC time as it will be on the previous day.
         self._create_radius_accounting(**options)
         response = self.client.get(reverse('admin:index'))
         self.assertEqual(response.status_code, 200)
-        start_time = get_datetime_filter_start_date()
-        end_time = get_datetime_filter_stop_date()
+        start_time = get_datetime_filter_start_datetime()
+        end_time = get_datetime_filter_stop_datetime()
         self.assertContains(
             response,
             (
@@ -59,4 +73,20 @@ class TestDeviceAdmin(CreateRadiusObjectsMixin, CreateDeviceMonitoringMixin, Tes
                 f'{start_time}&start_time__lt={end_time}\''
                 '}'
             ),
+        )
+
+    def test_radius_dashboard_chart_filter_url(self):
+        path = reverse(f'admin:{self.radius_label}_radiusaccounting_changelist')
+        response = self.client.get(
+            '{path}?start_time__gte={start_time}&start_time__lt={stop_time}'.format(
+                path=path,
+                start_time=get_datetime_filter_start_datetime(),
+                stop_time=get_datetime_filter_stop_datetime(),
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.request['QUERY_STRING'], 'e=1')
+        self.assertContains(
+            response,
+            '<div class="selected-option" tabindex="0" title="Today">',
         )
