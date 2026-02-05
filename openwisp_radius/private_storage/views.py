001a0c717977ac66e61f742737ec56dd3dbbcb94
Sarthak Tyagi <142912014+stktyagi@users.noreply.github.com>
2026-02-05 22:39:52 +0530

    [fix] Added CSV batch download endpoint to REST API docs #373
    Closes #373

--- openwisp_radius/private_storage/views.py
+++ openwisp_radius/private_storage/views.py
@@ -1,4 +1,8 @@
+from django.utils.translation import gettext_lazy as _
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from private_storage.views import PrivateStorageDetailView
+from rest_framework.views import APIView
 
 from ..settings import PRIVATE_STORAGE_INSTANCE
 from ..utils import load_model
@@ -10,6 +14,7 @@ class RadiusBatchCsvDownloadView(PrivateStorageDetailView):
     model_file_field = "csvfile"
     slug_field = "csvfile"
     slug_url_kwarg = "path"
+    pk_url_kwarg = "pk"
 
     def can_access_file(self, private_file):
         user = private_file.request.user
@@ -19,3 +24,24 @@ class RadiusBatchCsvDownloadView(PrivateStorageDetailView):
 
 
 rad_batch_csv_download_view = RadiusBatchCsvDownloadView.as_view()
+
+
+class RadiusBatchCsvDownloadAPIView(APIView):
+    @swagger_auto_schema(
+        operation_id="radius_organization_batch_csv_read",
+        operation_description=_(
+            "Allows downloading the CSV file used to import users for a "
+            "specific batch user creation operation."
+        ),
+        responses={
+            200: openapi.Response(
+                description=_("CSV file"), schema=openapi.Schema(type=openapi.TYPE_FILE)
+            ),
+        },
+        tags=["radius"],
+    )
+    def get(self, request, _slug, pk, *args, **kwargs):
+        return rad_batch_csv_download_view(request, pk=pk, **kwargs)
+
+
+rad_batch_csv_download_api_view = RadiusBatchCsvDownloadAPIView.as_view()
