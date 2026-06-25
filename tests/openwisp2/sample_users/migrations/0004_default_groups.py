49420ddd0a0c586a67d06250f1b709219deec7c7
Federico Capoano <f.capoano@openwisp.io>
2026-06-24 23:22:41 -0300

    [tests:fix] Added ApiKey proxy model and permissions to sample users

--- tests/openwisp2/sample_users/migrations/0004_default_groups.py
+++ tests/openwisp2/sample_users/migrations/0004_default_groups.py
@@ -4,6 +4,7 @@ import swapper
 from django.db import migrations
 
 from openwisp_users.migrations import (
+    add_api_key_permissions_to_admins,
     allow_admins_change_organization,
     create_default_groups,
     set_default_organization_uuid,
@@ -14,7 +15,7 @@ from openwisp_users.migrations import (
 class Migration(migrations.Migration):
     org_model = swapper.get_model_name("openwisp_users", "organization")
     dependencies = [
-        swapper.dependency(*swapper.split(org_model), version="0001_initial")
+        swapper.dependency(*swapper.split(org_model), version="0002_apikey")
     ]
 
     operations = [
@@ -29,5 +30,8 @@ class Migration(migrations.Migration):
         ),
         migrations.RunPython(
             allow_admins_change_organization, reverse_code=migrations.RunPython.noop
+        ),
+        migrations.RunPython(
+            add_api_key_permissions_to_admins, reverse_code=migrations.RunPython.noop
         ),
     ]
