b315e849955c5b586aee6e9519f00527c963f464
Federico Capoano <f.capoano@openwisp.io>
2026-06-30 10:31:45 -0300

    [change] Monitoring: trigger TSDB save on sessions close
    The integration with OpenWISP Monitoring now is triggered
    exclusively when RADIUS accounting sessions close, instead
    of the previous approach which used the generic post_save
    signal and used custom logic to determine if the RADIUS
    session was being closed or not.

--- openwisp_radius/integrations/monitoring/receivers.py
+++ openwisp_radius/integrations/monitoring/receivers.py
@@ -1,11 +1,15 @@
 from django.db import transaction
+from django.utils import timezone
 
 from . import tasks
 
 
-def post_save_radiusaccounting(instance, *args, **kwargs):
+def _enqueue_radiusaccounting_metric(instance):
     if instance.stop_time is None:
         return
+    metric_time = instance.stop_time
+    if timezone.is_naive(metric_time):
+        metric_time = timezone.make_aware(metric_time)
     transaction.on_commit(
         lambda: tasks.post_save_radiusaccounting.delay(
             username=instance.username,
@@ -14,5 +18,10 @@ def post_save_radiusaccounting(instance, *args, **kwargs):
             output_octets=instance.output_octets,
             calling_station_id=instance.calling_station_id,
             called_station_id=instance.called_station_id,
+            time=metric_time,
         )
     )
+
+
+def radius_accounting_closed_handler(instance, *args, **kwargs):
+    _enqueue_radiusaccounting_metric(instance)
